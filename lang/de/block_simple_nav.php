978e5bdbd3285f83ad4f9e57c02b060c0d5158e3
David Bogner <davidbogner@gmail.com>
2014-01-19 22:05:49 +0100

    german language file added

---
+++ lang/de/block_simple_nav.php
@@ -0,0 +1,28 @@
+<?php
+
+	
+    $string['pluginname'] = 'Simple navigation block';
+    $string['simple_nav'] = 'Einfache Navigation';
+    $string['simple_nav:addinstance'] = 'Block Einfach Navigation hinzufügen';
+    $string['blockstring'] = 'Titel';
+    $string['sn_home'] = 'Home: Leer lassen, um Moodle Standardeinstellung zu verwenden';
+    $string['sn_blocktitle'] = 'Blocküberschrift: Leer für Standardeinstellung';
+    $string['sn_space'] = 'Zeichen für Texteinrückung';
+    $string['sn_show_subcategories'] = 'Unterkategorien anzeigen';
+    $string['sn_show_courses'] = 'Kurse anzeigen';
+    $string['sn_show_modules'] = 'Module/Aktivitäten anzeigen';
+    $string['sn_modules_in_courses'] = 'Module/Aktivitäten in Kursen anzeigen';
+    $string['sn_modules_on_frontpage'] = 'Module/Aktivitäten der Startseite anzeigen';
+    $string['sn_startcategory'] = 'Anzuzeigende Kategorien';
+    $string['sn_show_toplevelnode'] = 'Übergeordnete Kategorie anzeigen (changing this might cause inactive nodes to be invisible)';
+    $string['displaywithicons'] = 'Navigationsbaum mit Standard Moodle-Theme Icons darstellen';
+    $string['displaysingleicon'] = 'Nur ► als Icon für alle Elemente des Baumes verwenden';
+    $string['displaytwoicons'] = '► Kurse und Kursbereiche ▪ für Aktivitäten';
+    $string['displayoptions'] = 'Darstellungsoptionen für Aufzählungszeichen';
+    $string['displayoptionsdesc'] = 'Wählen Sie unter folgenden Aufzählungszeichen: Moodle Standard mit Aktivitätssymbolen, nur ►, ► und ▪';
+    
+    
+    // if new modules are added, the language files have to be included
+    $string['sn_show_mods'] = 'Anzeigen ';
+    $string['simple_nav:viewcourse'] = 'Kurse ansehen';
+    
