c18e74916aee2364a2c534423d068b06a7a25d53
David Bogner <davidbogner@gmail.com>
2014-01-19 22:06:16 +0100

    bullet points display options added

---
+++ settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Block simple_nav
+ *
+ * @package    block_simple_nav
+ * @copyright  Georg Maißer, David Bogner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+	$name = 'block_simple_nav/displayoptions';
+	$visiblename = get_string('displayoptions', 'block_simple_nav');
+	$description = get_string('displayoptionsdesc', 'block_simple_nav');
+	$defaultsetting = 1;
+	$choices = array(1 => get_string('displaywithicons', 'block_simple_nav'), 2 => get_string('displaysingleicon', 'block_simple_nav'), 3 => get_string('displaytwoicons', 'block_simple_nav'));
+	$setting = new admin_setting_configselect($name, $visiblename, $description, $defaultsetting, $choices);
+	$settings->add($setting);
+}
